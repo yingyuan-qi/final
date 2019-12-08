311506cc53ddadb87573c44d2340cc0791e41f74
Yingyuan Qi <yq22732@columbia.edu>
2019-12-08 22:52:46 +0000

    Update models

--- sightings/models.py
+++ sightings/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 class Sightings(models.Model):
-    latitude = models.FloatField(default=0.0)
-    longitude = models.FloatField(default=0.0)
+    latitude = models.FloatField(null=True,)
+    longitude = models.FloatField(null=True,)
     unique_squirrel_id = models.CharField(max_length=100, primary_key=True)
     AM = 'AM'
     PM = 'PM'
@@ -14,9 +14,10 @@ class Sightings(models.Model):
     shift = models.CharField(
             max_length=3,
             choices=SHIFT_CHOICES,
-            default='AM',
+            default=AM,
+            null=True,
     )
-    date=models.DateField(help_text=_('Date'), null=True, blank=True,)
+    date=models.DateField(null=True, blank=True,)
     ADULT = 'Adult'
     JUVENILE = 'Juvenile'
     AGE_CHOICES = (
@@ -27,6 +28,7 @@ class Sightings(models.Model):
             max_length=10,
             choices=AGE_CHOICES,
             default='',
+            null=True,
     )
     GRAY = 'Gray'
     BLACK = 'Black'
@@ -40,6 +42,7 @@ class Sightings(models.Model):
             max_length=10,
             choices=FUR_CHOICES,
             default='',
+            null=True,
     )
 
     GROUND_PLANE = 'Ground Plane'
@@ -52,16 +55,21 @@ class Sightings(models.Model):
             max_length=20,
             choices=LOC_CHOICES,
             default='',
+            null=True,
     )
 
-    specific_location=models.CharField(max_length=100,default='')
+    specific_location=models.CharField(
+            max_length=100,
+            default='',
+            null=True,
+    )
 
     running=models.BooleanField()
     chasing=models.BooleanField()
     climbing=models.BooleanField()
     eating=models.BooleanField()
     foraging=models.BooleanField()
-    other_activities=models.CharField(max_length=100)
+    other_activities=models.CharField(max_length=100, default='', null=True,)
     kuks=models.BooleanField()
     quaas=models.BooleanField()
     moans=models.BooleanField()
